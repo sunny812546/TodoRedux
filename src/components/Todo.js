6964d0d14edcd83da3b17909e32855053de13b17
sunny812546 <sun812546@gmail.com>
2020-09-30 12:27:56 +0530

    hello

---
+++ src/components/Todo.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+//redux
+import { connect } from "react-redux";
+import { removeTodo } from "../action/todo";
+
+const Todo = ({ todos, markComplete }) => {
+  return (
+    <ListGroup className="mt-5 mb-2">
+      {todos.map((todo) => (
+        <ListGroupItem key={todo.id}>
+          {todo.title}
+          <span className="float-right" onClick={() => markComplete(todo.id)}>
+            ✅
+          </span>
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  markComplete: (id) => {
+    dispatch(removeTodo(id));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todo);
